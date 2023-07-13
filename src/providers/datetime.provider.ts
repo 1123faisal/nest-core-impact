e0316ac81e06a75f635ef6cba1f7a38f6609130b
Faisal Khan <faisal.khan@webmobriltechnologies.com>
2023-07-14 00:32:49 +0530

    push new changes

--- src/providers/datetime.provider.ts
+++ src/providers/datetime.provider.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as momentTimezone from 'moment-timezone';
+import * as moment from 'moment';
 
 @Injectable()
 export class DateTimeProvider {
@@ -10,7 +11,8 @@ export class DateTimeProvider {
    * @param dateStr '2023-07-22'
    * @param zone 'Asia/Kolkata'
    * @returns utcDateTime
-   * supported formates: YYYY-MM-DD | YYYY-MM-DD HH:mm | YYYY-MM-DD HH:mm:ss
+   * supported format's: YYYY-MM-DD | YYYY-MM-DD HH:mm | YYYY-MM-DD HH:mm:ss
+   * using moment-timezone because client sent date with time and to convert time to utc, we need timezone.
    */
   getUtc(dateStr: string, zone: string, format?: string) {
     const userDateTime = momentTimezone.tz(
@@ -20,5 +22,28 @@ export class DateTimeProvider {
     );
     const utcDateTime = userDateTime.utc();
     return utcDateTime;
+  }
+
+  /**
+   * there no need a moment-timezone because only date convert to utc doesn't need timezone.
+   * @returns start utc date time and end utc date time
+   */
+  getStartAndEndDayDateTime() {
+    // User's query date ("2023-07-12" in this example)
+    const userQueryDate = moment('2023-07-12').startOf('day');
+
+    // Convert the user's query date to UTC
+    const utcQueryDate = userQueryDate.utc();
+
+    // Calculate the UTC date range for the user's query
+    const startUTCDate = utcQueryDate.startOf('day');
+    const endUTCDate = utcQueryDate.endOf('day');
+
+    // example
+    // Event.find({
+    //   date: { $gte: startUTCDate.toDate(), $lte: endUTCDate.toDate() }
+    // }),
+
+    return { startUTCDate, endUTCDate };
   }
 }
